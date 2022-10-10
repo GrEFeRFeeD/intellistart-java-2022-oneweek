76d0e59150a9cf2d7da42ceac38c58ef1f87bad2
GrEFeRFeeD <azofer77@gmail.com>
2022-10-10 23:44:23 +0300

    :memo: Add documentation for UserService.

--- src/main/java/com/intellias/intellistart/interviewplanning/model/user/UserService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/user/UserService.java
@@ -2,6 +2,9 @@ package com.intellias.intellistart.interviewplanning.model.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * Service for User entity.
+ */
 public class UserService {
 
   private final UserRepository userRepository;
@@ -11,6 +14,11 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
+  /**
+   * Method for gaining current authorized User object (not implemented yet).
+   *
+   * @return currently just newUser() without relationships.
+   */
   public User getCurrentUser() {
 
     return new User(null, "testuser@example.com", null, null);
