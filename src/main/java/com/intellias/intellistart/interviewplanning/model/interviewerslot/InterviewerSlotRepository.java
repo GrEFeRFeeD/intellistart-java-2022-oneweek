7a2c9132a8d86515a76149964400299e7289e824
Sasha Vasylenko <svasilenko389@gmail.com>
2022-10-21 17:39:18 +0300

    :sparkles: added new method for repo

--- src/main/java/com/intellias/intellistart/interviewplanning/model/interviewerslot/InterviewerSlotRepository.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/interviewerslot/InterviewerSlotRepository.java
@@ -1,10 +1,17 @@
 package com.intellias.intellistart.interviewplanning.model.interviewerslot;
 
+import com.intellias.intellistart.interviewplanning.model.dayofweek.DayOfWeek;
+import com.intellias.intellistart.interviewplanning.model.user.User;
+import com.intellias.intellistart.interviewplanning.model.week.Week;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * DAO for InterviewerSlot entity.
  */
+
 public interface InterviewerSlotRepository extends CrudRepository<InterviewerSlot, Long> {
 
+  List<InterviewerSlot> getInterviewerSlotsByUserAndWeekAndDayOfWeek(User user ,Week week, DayOfWeek dayOfWeek);
 }
