7cbda14bbc393c2b61e544c74c8efb0a2adbd644
Sonya <103060451+sonnka@users.noreply.github.com>
2022-10-25 22:58:05 +0300

    Week service (#18)
    * :sparkles: Create new methods in WeekService: getNumberOfWeek(),getDayOfWeek() and convertToLocalDate()
    
    * :sparkles: Add tests for methods of WeekService: getNumberOfWeek(), getDayOfWeek(), convertToLocalDate()
    
    * :sparkles: Implement GuestController (GET /weeks/current, GET /weeks/next)
    
    * :sparkles: Add tests for GuestController and WeekService
    
    * :bug: Fix validate errors
    
    * :bug: Fix tests errors, add documentation for methods, change GetMapping in GuestController
    
    * :recycle: Refactor some methods of WeekService
    
    * :recycle: Refactor tests for WeekService
    
    * :recycle: Refactor tests

--- src/main/java/com/intellias/intellistart/interviewplanning/controllers/GuestController.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controllers/GuestController.java
@@ -1,5 +1,9 @@
 package com.intellias.intellistart.interviewplanning.controllers;
 
+import com.intellias.intellistart.interviewplanning.model.week.Week;
+import com.intellias.intellistart.interviewplanning.model.week.WeekService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,4 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GuestController {
 
+  private final WeekService weekService;
+
+  @Autowired
+  public GuestController(WeekService weekService) {
+    this.weekService = weekService;
+  }
+
+  @GetMapping("weeks/current")
+  public Week currentWeek() {
+    return weekService.getCurrentWeek();
+  }
+
+  @GetMapping("weeks/next")
+  public Week nextWeek() {
+    return weekService.getNextWeek();
+  }
 }
