32539880229c5bea7ad46ffc6b08bdf1eba93fbe
makskostyshen <82410925+makskostyshen@users.noreply.github.com>
2022-10-25 21:12:58 +0300

    Period service (#17)
    * :sparkles: Create and test period converter, validators
    
    * :sparkles: Finish time validator
    
    * :sparkles: Complete OverlapService
    
    * :sparkles: Complete task: implement getPeriodMethod
    
    * :art: Improve styling to correspond to checkstyle
    
    * :white_check_mark: Write mocks to PeriodService, small refactor and styling
    
    * :white_check_mark: Add documentation to private methods
    
    * :white_check_mark: Fix tests and little refactor
    
    * :white_check_mark: Fix mock tests
    
    Co-authored-by: GrEFeRFeeD <66921192+GrEFeRFeeD@users.noreply.github.com>

--- src/main/java/com/intellias/intellistart/interviewplanning/model/period/PeriodService.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/period/PeriodService.java
@@ -1,5 +1,11 @@
 package com.intellias.intellistart.interviewplanning.model.period;
 
+import com.intellias.intellistart.interviewplanning.exceptions.InvalidBoundariesException;
+import com.intellias.intellistart.interviewplanning.model.period.services.OverlapService;
+import com.intellias.intellistart.interviewplanning.model.period.services.TimeConverter;
+import com.intellias.intellistart.interviewplanning.model.period.services.validation.PeriodValidator;
+import java.time.LocalTime;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +16,64 @@ import org.springframework.stereotype.Service;
 public class PeriodService {
 
   private final PeriodRepository periodRepository;
+  private final TimeConverter timeConverter;
+  private final PeriodValidator periodValidator;
+  private final OverlapService overlapService;
 
+  /**
+   * Constructor.
+   */
   @Autowired
-  public PeriodService(PeriodRepository periodRepository) {
+  public PeriodService(PeriodRepository periodRepository,
+      TimeConverter timeConverter,
+      PeriodValidator periodValidator,
+      OverlapService overlapService) {
+
     this.periodRepository = periodRepository;
+    this.timeConverter = timeConverter;
+    this.periodValidator = periodValidator;
+    this.overlapService = overlapService;
+  }
+
+
+  /**
+  * Get period by given parameters.
+  * If needed period exists obtain it,
+  * if not exists - create it
+  *
+  * @param fromString - String representation of lower time boundary
+  * @param toString - String representation of upper time boundary
+  *
+  * @throws InvalidBoundariesException when parameters are incorrect:
+  *     can't be read as time
+  *     wrong business logic
+  */
+  public Period getPeriod(String fromString, String toString) {
+    LocalTime from = timeConverter.convert(fromString);
+    LocalTime to = timeConverter.convert(toString);
+
+    periodValidator.validate(from, to);
+
+    Optional<Period> periodOptional = periodRepository.findPeriodByFromAndTo(from, to);
+
+    if (periodOptional.isPresent()) {
+      return periodOptional.get();
+    }
+    return createPeriod(from, to);
+  }
+
+  public boolean isOverlap(Period period1, Period period2) {
+    return overlapService.isOverlap(period1, period2);
+  }
+
+  /**
+   * Create period by from and to.
+   * Business logic of parameters is correct.
+   */
+  Period createPeriod(LocalTime from, LocalTime to) {
+    //TODO decide if remove double links between periods and slots
+
+    return periodRepository.save(
+        new Period(null, from, to, null, null, null));
   }
 }
