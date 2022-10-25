02655c78f132e5efcf87e00c9adc4a957a724342
sashav389 <74081970+sashav389@users.noreply.github.com>
2022-10-25 11:22:42 +0300

    Update InterviewPlanningApplication.java

--- src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/InterviewPlanningApplication.java
@@ -2,7 +2,6 @@ package com.intellias.intellistart.interviewplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import spring.main.web-application-type=none;
 
 /**
  * Main application.
