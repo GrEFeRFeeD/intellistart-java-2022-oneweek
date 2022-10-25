bdf8dbbd0b73e210ed0d1b9a22d427a20f52aa87
Sasha Vasylenko <svasilenko389@gmail.com>
2022-10-26 01:40:52 +0300

    :art: changed one string for code-style

--- src/main/java/com/intellias/intellistart/interviewplanning/model/interviewerslot/InterviewerSlotDtoValidator.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/interviewerslot/InterviewerSlotDtoValidator.java
@@ -165,7 +165,9 @@ public class InterviewerSlotDtoValidator {
 
     if (!interviewerSlotsList.isEmpty()) {
       for (InterviewerSlot interviewerSlot : interviewerSlotsList) {
-//TODO use with ready period service if(PeriodService.isOverlap(interviewerSlot.getPeriod(), period)){return true;}
+        //TODO use with ready period service
+        // if(PeriodService.isOverlap(interviewerSlot.getPeriod(), period)){return true;}
+
       }
     }
     return false;
