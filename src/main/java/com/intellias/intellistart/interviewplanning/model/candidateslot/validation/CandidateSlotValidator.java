eacf1edbb8e2e72f28ec38e98ee63653d09bc59e
Artur Guzey <artur.guzey@gmail.com>
2022-11-18 02:53:08 +0200

    :hammer: Update the logic in validation without using the keyword "continue" int CandidateSlotValidator.validateOverlapping;

--- src/main/java/com/intellias/intellistart/interviewplanning/model/candidateslot/validation/CandidateSlotValidator.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/model/candidateslot/validation/CandidateSlotValidator.java
@@ -91,7 +91,7 @@ public class CandidateSlotValidator {
 
     if (!candidateSlotList.isEmpty()) {
       for (CandidateSlot item : candidateSlotList) {
-        if (!candidateSlot.getId().equals(item.getId())
+        if (!(candidateSlot.getId() != null && candidateSlot.getId().equals(item.getId()))
                 && periodService.areOverlapping(period, item.getPeriod())) {
           throw new SlotIsOverlappingException();
         }
