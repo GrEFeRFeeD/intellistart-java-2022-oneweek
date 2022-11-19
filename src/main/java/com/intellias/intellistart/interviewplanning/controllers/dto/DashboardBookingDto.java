e0b20d3daa86e98bb881d2cdcb150ae327e14aaa
GrEFeRFeeD <azofer77@gmail.com>
2022-11-19 15:41:59 +0200

    :white_check_mark: Add test for DashboardBookingDto object.

--- src/main/java/com/intellias/intellistart/interviewplanning/controllers/dto/DashboardBookingDto.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/controllers/dto/DashboardBookingDto.java
@@ -1,6 +1,7 @@
 package com.intellias.intellistart.interviewplanning.controllers.dto;
 
 import com.intellias.intellistart.interviewplanning.model.booking.Booking;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -37,5 +38,27 @@ public class DashboardBookingDto {
     this.candidateSlotId = booking.getCandidateSlot().getId();
     this.from = booking.getPeriod().getFrom().toString();
     this.to = booking.getPeriod().getTo().toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    DashboardBookingDto that = (DashboardBookingDto) o;
+    return Objects.equals(bookingId, that.bookingId) && Objects.equals(subject,
+        that.subject) && Objects.equals(description, that.description)
+        && Objects.equals(interviewerSlotId, that.interviewerSlotId)
+        && Objects.equals(candidateSlotId, that.candidateSlotId)
+        && Objects.equals(from, that.from) && Objects.equals(to, that.to);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(bookingId, subject, description, interviewerSlotId, candidateSlotId, from,
+        to);
   }
 }
