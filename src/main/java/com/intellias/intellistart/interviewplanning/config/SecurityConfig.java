9315320b2c773af499bb08830e27d0cede48eecf
GrEFeRFeeD <azofer77@gmail.com>
2022-11-19 14:39:09 +0200

    :lock: Add roles requiring for endpoints.

--- src/main/java/com/intellias/intellistart/interviewplanning/config/SecurityConfig.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/config/SecurityConfig.java
@@ -69,10 +69,12 @@ public class SecurityConfig {
 
     http.csrf().disable()
 
-        .authorizeRequests().antMatchers("/authenticate").permitAll()
+        .authorizeRequests().antMatchers("/authenticate",
+            "/weeks/current", "/weeks/next").permitAll()
         .antMatchers("/candidates/**").hasRole("CANDIDATE")
         .antMatchers("/interviewers/**").hasAnyRole("INTERVIEWER", "COORDINATOR")
-        .antMatchers("/bookings", "/bookings/**", "/users", "/users/**").hasRole("COORDINATOR")
+        .antMatchers("/bookings", "/bookings/**", "/users", "/users/**",
+            "/weeks/{weekNum}/dashboard").hasRole("COORDINATOR")
         .anyRequest().authenticated().and()
 
         // Add custom handling for unauthenticated and access denied errors
