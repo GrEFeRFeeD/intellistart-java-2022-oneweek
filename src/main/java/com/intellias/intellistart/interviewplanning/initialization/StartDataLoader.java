d5af46c4aaaffaedfd92851a1d4332858afc5bed
Maks Kostyshen <maxxxymko2003@gmail.com>
2022-11-21 16:41:27 +0300

    :sparkles: add initial data

--- src/main/java/com/intellias/intellistart/interviewplanning/initialization/StartDataLoader.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/initialization/StartDataLoader.java
@@ -1,8 +1,23 @@
 package com.intellias.intellistart.interviewplanning.initialization;
 
+import com.intellias.intellistart.interviewplanning.model.candidateslot.CandidateSlot;
+import com.intellias.intellistart.interviewplanning.model.candidateslot.CandidateSlotRepository;
+import com.intellias.intellistart.interviewplanning.model.candidateslot.CandidateSlotService;
+import com.intellias.intellistart.interviewplanning.model.dayofweek.DayOfWeek;
+import com.intellias.intellistart.interviewplanning.model.interviewerslot.InterviewerSlot;
+import com.intellias.intellistart.interviewplanning.model.interviewerslot.InterviewerSlotRepository;
+import com.intellias.intellistart.interviewplanning.model.interviewerslot.InterviewerSlotService;
+import com.intellias.intellistart.interviewplanning.model.period.Period;
+import com.intellias.intellistart.interviewplanning.model.period.PeriodService;
 import com.intellias.intellistart.interviewplanning.model.user.Role;
 import com.intellias.intellistart.interviewplanning.model.user.User;
 import com.intellias.intellistart.interviewplanning.model.user.UserRepository;
+import com.intellias.intellistart.interviewplanning.model.user.UserService;
+import com.intellias.intellistart.interviewplanning.model.week.Week;
+import com.intellias.intellistart.interviewplanning.model.week.WeekService;
+
+import java.time.LocalDate;
+import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -16,13 +31,28 @@ import org.springframework.stereotype.Component;
 public class StartDataLoader implements ApplicationRunner {
 
   private final UserRepository userRepository;
+  private final InterviewerSlotRepository interviewerSlotRepository;
+  private final CandidateSlotRepository candidateSlotRepository;
+  private final UserService userService;
+  private final InterviewerSlotService interviewerSlotService;
+  private final PeriodService periodService;
+  private final WeekService weekService;
 
   @Value("${first-coordinator-email}")
   private String email;
 
   @Autowired
-  public StartDataLoader(UserRepository userRepository) {
+  public StartDataLoader(UserRepository userRepository, InterviewerSlotRepository interviewerSlotRepository, CandidateSlotRepository candidateSlotRepository, UserService userService,
+      InterviewerSlotService interviewerSlotService, PeriodService periodService,
+      WeekService weekService) {
     this.userRepository = userRepository;
+    this.interviewerSlotRepository = interviewerSlotRepository;
+    this.candidateSlotRepository = candidateSlotRepository;
+    this.userService = userService;
+    this.interviewerSlotService = interviewerSlotService;
+    this.periodService = periodService;
+
+    this.weekService = weekService;
   }
 
   @Override
@@ -31,8 +61,26 @@ public class StartDataLoader implements ApplicationRunner {
     User firstCoordinator = new User(null, email, Role.COORDINATOR);
     firstCoordinator = userRepository.save(firstCoordinator);
 
+    User firstInterviewer = userService.grantRoleByEmail("testing@gmail.com", Role.INTERVIEWER);
+    User secondInterviewer = userService.grantRoleByEmail("second@gmail.com", Role.INTERVIEWER);
 
-    // Add needed repositories to this class and save any information you want
-    // First coordinator will be with email azofer77@gmail.com
+    DayOfWeek dayOfWeek = DayOfWeek.TUE;
+    Week next = weekService.getNextWeek();
+    LocalDate date = weekService.convertToLocalDate(next.getId(), dayOfWeek);
+
+    Period periodInterviewer1 = periodService.obtainPeriod("12:00", "18:00");
+    Period periodCandidate1 = periodService.obtainPeriod("12:00", "21:30");
+
+    InterviewerSlot interviewerSlot1 = new InterviewerSlot(null, next, dayOfWeek, periodInterviewer1, new HashSet<>(), firstInterviewer);
+    InterviewerSlot interviewerSlot1Saved = interviewerSlotRepository.save(interviewerSlot1);
+
+    CandidateSlot candidateSlot1 = new CandidateSlot(null, date, periodCandidate1, new HashSet<>(),
+            "candidate1@gmail.com", "Maks");
+    CandidateSlot candidateSlot1Saved = candidateSlotRepository.save(candidateSlot1);
+
+
+
+
+    System.out.println("data is loaded");
   }
 }
