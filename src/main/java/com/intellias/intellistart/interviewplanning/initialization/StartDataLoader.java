03aa9a9be925615e9a5c5ecb35fe99883bd68840
sonnka <sonyakazanceva1331@gmail.com>
2022-11-21 14:37:53 +0200

    :art: Add initial data for the demo

--- src/main/java/com/intellias/intellistart/interviewplanning/initialization/StartDataLoader.java
+++ src/main/java/com/intellias/intellistart/interviewplanning/initialization/StartDataLoader.java
@@ -1,8 +1,14 @@
 package com.intellias.intellistart.interviewplanning.initialization;
 
+import com.intellias.intellistart.interviewplanning.model.bookinglimit.BookingLimit;
+import com.intellias.intellistart.interviewplanning.model.bookinglimit.BookingLimitKey;
+import com.intellias.intellistart.interviewplanning.model.bookinglimit.BookingLimitRepository;
 import com.intellias.intellistart.interviewplanning.model.user.Role;
 import com.intellias.intellistart.interviewplanning.model.user.User;
 import com.intellias.intellistart.interviewplanning.model.user.UserRepository;
+import com.intellias.intellistart.interviewplanning.model.week.Week;
+import com.intellias.intellistart.interviewplanning.model.week.WeekRepository;
+import java.util.HashSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -16,13 +22,18 @@ import org.springframework.stereotype.Component;
 public class StartDataLoader implements ApplicationRunner {
 
   private final UserRepository userRepository;
+  private final WeekRepository weekRepository;
+  private final BookingLimitRepository bookingLimitRepository;
 
   @Value("${first-coordinator-email}")
   private String email;
 
   @Autowired
-  public StartDataLoader(UserRepository userRepository) {
+  public StartDataLoader(UserRepository userRepository, WeekRepository weekRepository,
+      BookingLimitRepository bookingLimitRepository) {
     this.userRepository = userRepository;
+    this.weekRepository = weekRepository;
+    this.bookingLimitRepository = bookingLimitRepository;
   }
 
   @Override
@@ -30,6 +41,20 @@ public class StartDataLoader implements ApplicationRunner {
 
     User firstCoordinator = new User(null, email, Role.COORDINATOR);
     firstCoordinator = userRepository.save(firstCoordinator);
+
+    User firstInterviewer = new User(null,"sonyakazanceva1331@gmail.com",Role.INTERVIEWER);
+    firstInterviewer = userRepository.save(firstInterviewer);
+
+    User secondInterviewer = new User(null,"email@gmail.com",Role.INTERVIEWER);
+    secondInterviewer = userRepository.save(secondInterviewer);
+
+    Week week = new Week(40L,new HashSet<>());
+    week = weekRepository.save(week);
+
+    BookingLimit bookingLimit = new BookingLimit(
+        new BookingLimitKey(firstInterviewer.getId(), week.getId()), 17, firstInterviewer,
+        week);
+    bookingLimit = bookingLimitRepository.save(bookingLimit);
 
 
     // Add needed repositories to this class and save any information you want
